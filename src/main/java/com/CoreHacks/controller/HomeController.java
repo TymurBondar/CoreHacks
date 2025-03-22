6d60223c91cd61c59942ec34dff637561702c717
Tymur Bondar <tymurbondar@outlook.com>
2025-03-22 18:28:21 -0400

    add popup

--- src/main/java/com/CoreHacks/controller/HomeController.java
+++ src/main/java/com/CoreHacks/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.CoreHacks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+public class HomeController {
 
     @GetMapping("/")
     public String index() {
         return "index"; // will render templates/index.html
     }
+
+    @Controller
+    public class PopupController {
+
+        @GetMapping("/popup")
+        public String showPopupForm(Model model) {
+            return "popup-form"; // popup-form.html
+        }
+    }
+
 }
